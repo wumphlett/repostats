ced264a2f897ad24db02e4491cb65fd287c1af6a
Will Humphlett <will@humphlett.net>
2022-08-25 01:21:13 -0500

    Adjust readme naming collision

--- repostats/readme.py
+++ repostats/readme.py
@@ -9,7 +9,7 @@ def main():
     workplace_path = Path(os.getenv("GITHUB_WORKSPACE")).resolve() / ".traffic"
     github_path = Path(os.getenv("GITHUB_WORKSPACE")).resolve() / ".github"
 
-    readme_path = next((path for path in github_path.iterdir() if path.stem.startswith("README")), None)
+    readme_path = next((path for path in github_path.iterdir() if path.stem.startswith("TEMPLATE_README")), None)
 
     if readme_path is None:
         raise Exception("Error: A README template must be present in the base of the .github directory")
@@ -25,7 +25,9 @@ def main():
 
     readme = readme_template.format(VIEWS_CHART=views_chart, CLONES_CHART=clones_chart)
 
-    with open(Path(os.getenv("GITHUB_WORKSPACE")).resolve() / readme_path.name, "w", encoding="utf-8") as readme_file:
+    readme_name = readme_path.name.replace("TEMPLATE_", "")
+
+    with open(Path(os.getenv("GITHUB_WORKSPACE")).resolve() / readme_name, "w", encoding="utf-8") as readme_file:
         readme_file.write(readme)
 
 
